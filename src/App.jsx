d2f06c0c87918f737caba023642d94f3f6f7abde
Mateo Alvarez <mateoalvarez384gmail.com>
2024-09-11 16:20:49 -0300

    import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Home } from "./components/home/Home";
 import { Solution } from "./components/solution/Solution";
 import { Testimonio } from "./components/testimonio/Testimonio";
 import { Carrousel } from "./components/carrousel/Carrousel";
-import { Preguntas } from "./components/F&Q/preguntas";
+import { Preguntas } from "./components/F&Q/Preguntas";
 import { Footer } from "./components/footer/Footer";
 import { Servicios } from "./components/servicios/Servicios";
 import { Fade } from "react-awesome-reveal";
